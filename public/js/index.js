600d349a38cda9eaa1c9eab349b256496d63b199
Massimo Codissimo <codissimo@gmail.com>
2018-02-21 20:28:49 -0800

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,6 @@ socket.on('disconnect', function() {
   console.log('disconnected');
 });
 
-socket.on('createwMessage', function(message) {
-  console.log('new message', message);
+socket.on('newMessage', function(message) {
+  console.log('newMessage', message);
 });
