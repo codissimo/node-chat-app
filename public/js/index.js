ffec9dfb2de5569081f5417f249dcbfd3dee3acc
Massimo Codissimo <codissimo@gmail.com>
2018-02-23 10:48:21 -0800

    Added autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   // console.log('connected');
 }); 
@@ -16,6 +32,7 @@ socket.on('newMessage', function(message) {
     text: message.text
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 
 //   // console.log('newMessage', message);
 //   var li = jQuery('<li></li>');
@@ -33,6 +50,7 @@ socket.on('newLocationMessage', function(message) {
     url: message.url
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 //   var li = jQuery('<li></li>');
 //   var a = jQuery('<a target="_blank">My current location</a>');
 //   li.text(`${moment.utc(message.createdAt).format('HH:mm:ss')} - ${message.from}: `);
